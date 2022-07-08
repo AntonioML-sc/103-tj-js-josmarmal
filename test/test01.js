9f51043b6dae6170023535d22c26ee434a6971b8
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-08 18:52:20 +0200

    third attempt: 2 test passed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,16 @@
-function tiers(index) {
-	return "";
+function tiers(num) {
+	let st = "";
+
+	for (let i = 1; i <= num; i++) {
+		for (let j = 1; j < num - i; j++) {
+			st += " ";			
+		}
+		for (let k = num - i; k < num; k++) {			
+			st += "*";
+		}
+		st += "\n";		
+	}
+
+	return st;
 }
 module.exports = tiers;
